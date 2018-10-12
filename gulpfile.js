9d68e1708427b0eed4c9f2d5cccf8b9171e40a5b
Laurent B <laurent@zantetsuken.fr>
2018-10-12 17:44:46 +0200

    update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-'use strict';
 
 // Requis
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	cssbeautify = require('gulp-cssbeautify'),
+	inlineCss = require('gulp-inline-css');
 var sass = require('gulp-sass');
+var csscomb = require('gulp-csscomb');
+const autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
 var extender = require('gulp-html-extend')
 
 // Include plugins
@@ -17,15 +21,44 @@ gulp.task('sass', function(){
   return gulp.src(source + '/scss/**/*.scss')
   	.pipe(plugins.sass())
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(plugins.csscomb())
+    .pipe(plugins.cssbeautify({indent: '  '}))
+    .pipe(plugins.autoprefixer())
+    .pipe(inlineCss({
+            	applyStyleTags: true,
+            	applyLinkTags: true,
+            	removeStyleTags: true,
+            	removeLinkTags: true
+        }))
     .pipe(gulp.dest(destination + '/css'))
 });
 
-gulp.task('extend', function () {
-    gulp.src(source + '/*.html')
-        .pipe(extender({annotations:true,verbose:false})) // default options
-        .pipe(gulp.dest(destination))
- 
-})
+gulp.task('minify', function () {
+  return gulp.src(destination + '/assets/css/*.css')
+    .pipe(plugins.csso())
+    .pipe(plugins.rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(destination + '/assets/css/'));
+});
+
+// Tâche "html" = includes HTML
+gulp.task('html', function() {
+  return  gulp.src(source + '/{,blocks/footer/2018/}/{,blocks/content/2018/}/{,blocks/header/}*.html')
+    // Generates HTML includes
+    .pipe(extender({
+      annotations: false,
+      verbose: false
+    })) // default options
+    .pipe(gulp.dest(destination))
+});
+
+gulp.task('inliner', function() {
+    return gulp.src(source + '/*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest(destination));
+});
+
 gulp.task('watch', function () {
-    gulp.watch([source + '/*.html'], ['extend'])
+    gulp.watch([destination + '/index.html'], ['html'], ['inliner'])
 })
